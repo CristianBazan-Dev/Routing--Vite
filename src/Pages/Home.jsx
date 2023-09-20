8e713581817dff81d029db12571c7fc0e2878c47
Cristian Bazan <johanwebdesigner@gmail.com>
2023-09-20 13:20:20 -0300

    Adding routes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
 
 function Home(props) {
     return (
-        <div>
-            
-        </div>
+        <article>
+            <Header/>
+      <main>
+            Home
+        </main>
+        <Footer/>
+        </article>
+  
+    
     );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './Pages/Home'
+import Products from './Pages/Products'
+import About from './Pages/About'
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Home/> 
+  }, 
+  {
+    path: "/products",
+    element: <Products/> 
+  },
+  {
+    path: "/about",
+    element: <About/> 
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
